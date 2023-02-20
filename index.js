3ed51d103a9ebab1afe500abb39ea5f79e947da2
Hong <gowipehong@gmail.com>
2023-02-20 11:11:50 +0800

    sign up endpoint setup done

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ import prisma from "./src/utils/prisma.js"
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', async (req, res) => {
-  const allUsers = await prisma.user.findMany()
-  res.json(allUsers)
-})
+app.use(express.json());
+
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body;
+
+  const newUser = await prisma.user.create({
+    data: {
+      name, 
+      email
+    },
+  });
+
+  res.json(newUser);
+});
 
 app.listen(port, () => {
   console.log(`App started; listening on port ${port}`)
