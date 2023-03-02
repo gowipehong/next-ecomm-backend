786ef9638f7513b717efca10396b80a0da9c0cdb
Hong <gowipehong@gmail.com>
2023-03-02 15:04:58 +0800

    Implemented display image endpoint

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import userRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
 import imagesRouter from "./src/controllers/image.controller.js"
-import auth from "./src/middlewares/auth.js"
+import recordsRouter from "./src/controllers/records.controller.js"
 import morgan from "morgan"
 const app = express()
 
@@ -14,6 +14,7 @@ app.use(express.json())
 app.use('/users', userRouter)
 app.use('/auth', authRouter)
 app.use('/images', imagesRouter)
+app.use('/imagepost', recordsRouter)
 
 export default app
 
